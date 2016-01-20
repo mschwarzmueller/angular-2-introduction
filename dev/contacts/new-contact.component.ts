48c8764c43d4139df11809a7c1f6518c4b0eea1b
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-01-20 15:25:35 +0100

    YouTube Course
    - Added first method of form validation (no form builder, two-way-binding)

--- dev/contacts/new-contact.component.ts
+++ dev/contacts/new-contact.component.ts
@@ -8,25 +8,43 @@ import {OnInit} from "angular2/core";
 
 @Component({
     template: `
-    <div>
+    <form #myForm="ngForm" (ngSubmit)="onSubmit()">
         <div>
             <label for="first-name">First Name:</label>
-            <input type="text" id="first-name" #firstName>
+            <input type="text" id="first-name"
+                ngControl="firstName"
+                [(ngModel)]="newContact.firstName"
+                required
+                #firstName="ngForm"
+            >
+            <span *ngIf="!firstName.valid">Not valid</span>
         </div>
             <label for="last-name">Last Name:</label>
-            <input type="text" id="last-name" #lastName value="{{passedLastName}}">
+            <input type="text" id="last-name"
+                ngControl="lastName"
+                [(ngModel)]="newContact.lastName"
+                required
+            >
         <div>
         </div>
         <div>
             <label for="phone">Phone Number:</label>
-            <input type="text" id="phone" #phone>
+            <input type="text" id="phone"
+                ngControl="phone"
+                [(ngModel)]="newContact.phone"
+                required
+            >
         </div>
         <div>
             <label for="email">E-Mail:</label>
-            <input type="text" id="email" #email>
+            <input type="text" id="email"
+                ngControl="email"
+                [(ngModel)]="newContact.email"
+                required
+            >
         </div>
-        <button (click)="onAddContact(firstName.value, lastName.value, phone.value, email.value)">Create Contact</button>
-    </div>
+        <button type="submit" [disabled]="!myForm.form.valid">Create Contact</button>
+    </form>
     `,
     providers: [ContactService],
     styles: [`
@@ -38,22 +56,24 @@ import {OnInit} from "angular2/core";
         input {
             width: 250px;
         }
+
+        .ng-invalid {
+            border: 1px solid red;
+        }
     `]
 })
 export class NewContactComponent implements OnInit
 {
-    public passedLastName = "";
+    newContact: Contact;
 
     constructor(private _contactService: ContactService, private _router: Router, private _routeParams: RouteParams) {}
 
-    onAddContact(firstName, lastName, phone, email) {
-        let contact: Contact = {firstName: firstName, lastName: lastName, phone: phone, email: email};
-        this._contactService.insertContact(contact);
+    onSubmit() {
+        this._contactService.insertContact(this.newContact);
         this._router.navigate(['Contacts']);
     }
 
-
     ngOnInit():any {
-        this.passedLastName = this._routeParams.get('lastName');
+        this.newContact = { firstName: '', lastName: this._routeParams.get('lastName'), phone: '', email: '' };
     }
 }
