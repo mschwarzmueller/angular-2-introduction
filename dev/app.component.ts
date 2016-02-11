01e5e7c52cb364ba5eceae1734657a766b46713a
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-02-11 22:00:30 +0100

    Cleaned up master

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,10 @@
 import {Component} from 'angular2/core';
-import {NewComponentComponent} from "./new-component.component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <new-component></new-component>
+        Hello World
     `,
-    directives: [NewComponentComponent]
 })
 export class AppComponent {
 
